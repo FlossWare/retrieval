be794c7d2ae3b2f90c34787799453d6dff378beb
Scot P. Floess <scot.floess@gmail.com>
2026-09-03 19:39:03 -0400

    feat: export Solr retriever

--- retrieval/__init__.py
+++ retrieval/__init__.py
@@ -1,6 +1,7 @@
 """Reusable retrieval capability."""
 
 from .in_memory import InMemoryRetriever
+from .solr import SolrRetriever
 from .types import RetrievalResult, Retriever, VectorRetriever
 
-__all__ = ["InMemoryRetriever", "RetrievalResult", "Retriever", "VectorRetriever"]
+__all__ = ["InMemoryRetriever", "RetrievalResult", "Retriever", "SolrRetriever", "VectorRetriever"]
